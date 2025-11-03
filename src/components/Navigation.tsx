e7a2fe96fb1bd7cbb53dc755242a4f208fdae387
JohnSin797 <jansenangelodolor@gmail.com>
2025-11-03 20:26:55 +0800

    change image search button bg color

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -183,7 +183,7 @@ const Navigation = () => {
                   <Button
                     variant="ghost"
                     size="sm"
-                    className="absolute right-1 top-1/2 transform -translate-y-1/2 h-8 w-8 p-0"
+                    className="absolute bg-sage-light right-1 top-1/2 transform -translate-y-1/2 h-8 w-8 p-0"
                     onClick={handleCameraClick}
                     disabled={isProcessingImage}
                   >
