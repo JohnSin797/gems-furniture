99e898930d702452c68c84cedd380a7f2c853ac8
JohnSin797 <jansenangelodolor@gmail.com>
2025-11-06 16:57:39 +0800

    adjustments on landing page image

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,10 +4,10 @@ const Hero = () => {
   return (
     <section className="relative min-h-screen flex items-center justify-center overflow-hidden">
       {/* Background Image */}
-      <div
-        className="absolute inset-0 bg-cover bg-center"
-        style={{ backgroundImage: `url(${heroImage})` }}
-      >
+       <div
+         className="absolute inset-0 bg-cover bg-center bg-no-repeat"
+         style={{ backgroundImage: `url(${heroImage})` }}
+       >
         <div className="absolute inset-0 bg-black/30"></div>
       </div>
 
